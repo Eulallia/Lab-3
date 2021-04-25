09af4189fe958bf84d42b080939c28ae1bd9231a
hayratu <hayratu@wp.pl>
2021-04-25 10:47:51 +0200

    Update ProjektZaliczeniowy.py

--- ProjektZaliczeniowy.py
+++ ProjektZaliczeniowy.py
@@ -4,14 +4,14 @@ def Oblicz_pole_figur(nazwa): \
     nazwa = nazwa.lower()
 
 
-    if nazwa == "prostokat":
+    if nazwa == "1":
         a = int(input("Podaj dlugosc prostokata: "))
         b = int(input("Podaj szerokosc prostokata: "))
 
         pole_prostokata = a * b
         print(f"Pole prostokata wynosi {pole_prostokata}.")
 
-    elif nazwa == "trojkat":
+    elif nazwa == "2":
         h = int(input("Podaj wysokosc trojkata: "))
         b = int(input("Podaj dlugosc podstawy trojkata: "))
 
@@ -19,14 +19,14 @@ def Oblicz_pole_figur(nazwa): \
         pole_trojkata = 0.5 * b * h
         print(f"Pole trojkata wynosi {pole_trojkata}.")
     
-    elif nazwa == "kwadrat":
+    elif nazwa == "3":
         s = int(input("Podaj dlugosc boku: "))
 
 
         pole_kwadratu = s * s
         print(f"Pole kwadratu wynosi {pole_kwadratu}. ")
 
-    elif nazwa == "kolo":
+    elif nazwa == "4":
         r = int(input("Podaj promień koła: "))
         pi = 3.14
 
@@ -39,11 +39,11 @@ def Oblicz_pole_figur(nazwa): \
 
 if __name__ == "__main__":
     print("Dostepne opcje:")
-    print("-prostokat")
-    print("-kwadrat")
-    print("-trojkat")
-    print("-kolo")
-    nazwa_ksztaltu = input("Podaj nazwe figury gemetrycznej dla ktorej chcesz obliczyc pole: ")
+    print("1.prostokat")
+    print("2.trojkat")
+    print("3.kwadrat")
+    print("4.kolo")
+    nazwa_ksztaltu = input("Podaj numer figury gemetrycznej dla ktorej chcesz obliczyc pole: ")
     
     Oblicz_pole_figur(nazwa_ksztaltu)
     input("Aby zakonczyc prace programu wcisnij ENTER ")
